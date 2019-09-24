7f6aea9841854aa58cf1d0044e9e3e2d4f5fea52
Nanoteck137 <patrik.millvik@gmail.com>
2019-09-24 18:57:54 +0200

    Basic javafx

--- Java/Flaggor/src/net/nanoteck137/flaggor/Main.java
+++ Java/Flaggor/src/net/nanoteck137/flaggor/Main.java
@@ -1,7 +1,26 @@
 package net.nanoteck137.flaggor;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Flaggor Uppgift");
+
+        Rectangle rect = new Rectangle(0.0f, 32.0f,  32.0f, 32.0f);
+
+        Group group = new Group();
+        group.getChildren().add(rect);
+
+        primaryStage.setScene(new Scene(group, 1280, 720));
+        primaryStage.show();
     }
 }
