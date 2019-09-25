fbd04ff2936d350ad0787bb1e15186433991295c
Nanoteck137 <patrik.millvik@gmail.com>
2019-09-25 18:48:55 +0200

    Började på en soviet flagga

--- Java/Flaggor/src/net/nanoteck137/flaggor/Main.java
+++ Java/Flaggor/src/net/nanoteck137/flaggor/Main.java
@@ -1,14 +1,18 @@
 package net.nanoteck137.flaggor;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.effect.Effect;
 import javafx.scene.effect.Glow;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -16,6 +20,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import net.nanoteck137.flaggor.flags.Flag;
 import net.nanoteck137.flaggor.flags.FlagGerman;
+import net.nanoteck137.flaggor.flags.FlagSoviet;
 import net.nanoteck137.flaggor.flags.FlagSwedish;
 
 import java.util.ArrayList;
@@ -33,28 +38,52 @@ public class Main extends Application {
         ArrayList<Flag> flags = new ArrayList<Flag>();
         flags.add(new FlagSwedish());
         flags.add(new FlagGerman());
+        flags.add(new FlagSoviet());
 
         FlowPane flagPane = new FlowPane();
         for(Flag flag : flags) {
             flagPane.getChildren().add(flag.render(0.0f, 0.0f, 1.0f));
         }
-        flagPane.setPrefWidth(1280 - 200);
 
-        Rectangle rect = new Rectangle(200, 720);
-        rect.setFill(new Color(1.0f, 0.0f, 1.0f, 1.0f));
+        float scale = 0.3f;
+        Polygon polygon = new Polygon();
+        polygon.getPoints().addAll(376.2 * scale, 3535.9* scale, -432.9* scale, 2948.1* scale, 59.8* scale, 3120.6* scale, 376.2* scale, 3535.9* scale,
+                567.1 * scale, 2948.1* scale, -432.9* scale, 2948.1* scale, 59.8* scale, 3133.7* scale, 567.1* scale, 2948.1* scale,
+                -241.9 * scale, 3535.9* scale, 567.1* scale, 2948.1* scale, 58.5* scale, 3089.9* scale, -241.9* scale, 3535.9* scale,
+                376.2 * scale, 3535.9* scale, 67.1* scale, 2584.8* scale, 15.0* scale, 3128.8* scale, 376.2* scale, 3535.9* scale,
+                -241.9 * scale, 3535.9* scale, 67.1* scale, 2584.8* scale, 99.6* scale, 3123.9* scale, -241.9* scale, 3535.9* scale,
+                376.2 * scale, 3535.9* scale, -432.9* scale, 2948.1* scale, 59.8* scale, 3120.6* scale, 376.2* scale, 3535.9* scale,
+                567.1 * scale, 2948.1* scale, -432.9* scale, 2948.1* scale, 59.8* scale, 3133.7* scale, 567.1* scale, 2948.1* scale,
+                -241.9 * scale, 3535.9* scale, 567.1* scale, 2948.1* scale, 58.5* scale, 3089.9* scale, -241.9* scale, 3535.9* scale,
+                376.2 * scale, 3535.9* scale, 67.1* scale, 2584.8* scale, 15.0* scale, 3128.8* scale, 376.2* scale, 3535.9* scale,
+                -241.9 * scale, 3535.9* scale, 67.1* scale, 2584.8* scale, 99.6* scale, 3123.9* scale, -241.9* scale, 3535.9* scale);
+
+        flagPane.getChildren().add(polygon);
+
+        flagPane.setPrefWidth(1280 - 200);
 
         Font font = new Font("Verdana", 24);
 
-        Label label = new Label("Test Label");
-        label.setFont(font);
-        label.setPrefWidth(200);
-        label.setTextAlignment(TextAlignment.RIGHT);
-        label.setMinWidth(200);
-
-        VBox panel = new VBox(label);
+        VBox panel = new VBox();
         panel.setMinWidth(200);
         panel.setFillWidth(true);
         panel.setCenterShape(true);
+
+        Label label = new Label("Test Label");
+        label.setFont(font);
+        label.setTextAlignment(TextAlignment.RIGHT);
+        panel.getChildren().add(label);
+
+        Slider slider = new Slider(0.5f, 4.0f, 1.0f);
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println(newValue);
+            flagPane.getChildren().clear();
+
+            for(Flag flag : flags) {
+                flagPane.getChildren().add(flag.render(0.0f, 0.0f, newValue.floatValue()));
+            }
+        });
+        panel.getChildren().add(slider);
 
         for(Flag flag : flags) {
             Label flagName = new Label(flag.getName());
