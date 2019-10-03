f8ae31407da983488455c9d821b58378b30537ca
Nanoteck137 <patrik.millvik@gmail.com>
2019-10-03 20:31:25 +0200

    Började på en Parser

---
+++ C-Sharp/Calc/Parser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Node
+{
+
+}
+
+class NodeNumber
+{
+    public double Number { get; private set; }
+
+    public NodeNumber(double number)
+    {
+        Number = number;
+    }
+};
+
+enum Operation
+{
+    PLUS,
+    MINUS,
+    DIVIDE,
+    MULTIPLY,
+}
+
+class NodeOp
+{
+    public Node Left { get; private set; }
+    public Node Right { get; private set; }
+    public Operation Op { get; private set; }
+
+    public NodeOp(Node left, Node right, Operation op)
+    {
+        Left = left;
+        Right = right;
+        Op = op;
+    }
+}
+
+class Parser
+{
+    private Lexer lexer;
+
+    public Parser(Lexer lexer)
+    {
+        this.lexer = lexer;
+    }
+
+    public Node Parse()
+    {
+        Node node = new Node();
+
+        return node;
+    }
+}
